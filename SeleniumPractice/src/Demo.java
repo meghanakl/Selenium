1b2fb1a57e2b3141547339d0a581ac78f60637fa
meghanaraokl <meghanaraokl@gmail.com>
2020-04-23 15:35:25 +0530

    commit

---
+++ SeleniumPractice/src/Demo.java
@@ -0,0 +1,19 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+	static {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\MeghanaRao\\Chrome\\chromedriver_win32\\chromedriver.exe");
+
+	}
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		String whR = driver.getWindowHandle();
+		System.out.println("Reference of current window==>" + whR);
+
+	}
+
+}
