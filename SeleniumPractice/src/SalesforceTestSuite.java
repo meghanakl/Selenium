75de812c8e9dc6df221033806855e8a9d83daa16
Vishwasa Rao <vishwasraokl@gmail.com>
2020-04-24 15:09:02 +0100

    added jars

--- SeleniumPractice/src/SalesforceTestSuite.java
+++ SeleniumPractice/src/SalesforceTestSuite.java
@@ -4,9 +4,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SalesforceTestSuite {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		String path = System.getProperty("user.dir");
 		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\MeghanaRao\\Chrome\\chromedriver_win32\\chromedriver.exe");
+				path+"/src/drivers/chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://login.salesforce.com/");
